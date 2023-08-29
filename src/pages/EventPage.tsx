206b42f485ed91b0130999d4a4eecc745b38c8db
kimjisub <0226daniel@gmail.com>
2023-08-30 02:11:16 +0900

    feat: many things

--- src/pages/EventPage.tsx
+++ src/pages/EventPage.tsx
@@ -3,11 +3,10 @@ import React from 'react';
 import { useNavigate, useParams } from 'react-router';
 import { makeApply, subscribeEvent } from '../db/firestore';
 import { EventDeclaration } from '../models/Event';
-import EventFieldPage from './ProgressPage/EventFieldPage';
 import { Button } from '@mui/material';
 
 function App() {
-	const { eventId } = useParams();
+	const { eventId = '' } = useParams();
 	const navigate = useNavigate();
 
 	const [event, setEvent] = React.useState<EventDeclaration | null>(null);
@@ -22,10 +21,8 @@ function App() {
 	}, [eventId]);
 
 	const goToNextPage = React.useCallback(async () => {
-		if (!eventId) return;
-		const applicationId = await makeApply(eventId);
-		navigate(`./applies/${applicationId}`);
-	}, [navigate, eventId]);
+		navigate(`./apply`);
+	}, [navigate]);
 
 	if (!event) return <div>loading...</div>;
 
